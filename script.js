21cc46a190b4e2f6d3df48c21a94240d99e70af0
alissonsc <alisson.csilva16@senacsp.edu.br>
2024-08-20 21:51:03 -0300

    feat: Calculando a média das notas do aluno

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+//Exibir no terminal
 console.log("Olá Mundo");
-
+//Exibir no navegador
 document.write("Olá Navegador !");
+//Um alerta
+alert("Alerta de Confirmação!");
+
+//Utilizar duas barras é um comentario dentro do terminal
+/*
+
+utilizar o barra asterístico é comentario em grande
+quanntidade ou bloco inteiro 
+*/
+
+//string
+
+//Capturar o nome do Aluno
+var nome = window.prompt("Informe o nome:");
+
+// Guardar a primeira nota
+var nota1 = window.prompt("Digite a nota 1:");
+
+//converte a nota1 de texto para decimal
+nota1 = parseFloat(nota1);
+
+//Guardar a segunda nota
+var nota2 = window.prompt("Digite a nota 2:");
+
+//converte a nota2 de texto para decimal
+nota2 = parseFloat(nota2);
+
+//calculo a média das duas notas
+var media = (nota1 + nota2)/2;
+
+//Gero a mensagem de exibição do nome e da média do aluno
+var mensagem = nome + " , você obteve a média : " + media;
+window.alert(mensagem);
+
+
+window.alert("A média das notas é: " + media);
